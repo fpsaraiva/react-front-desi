de424474e3ace608a246c036a6d23a933918c122
Fernando Saraiva <fpsaraiva@gmail.com>
2024-11-06 21:52:23 -0300

    inicio criacao router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import ErrorPage from './error-page'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Bem vindo!</div>,
+    errorElement: <ErrorPage />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
